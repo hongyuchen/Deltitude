8b5d1295a6088a645512843026c234158f10aa47
Hongyu Chen <hongyu.chen.15@dartmouth.edu>
2015-04-11 18:17:39 -0400

    Mode one done!

--- src/main.c
+++ src/main.c
@@ -11,9 +11,14 @@
 #define SAMPLE_RATE 10
 
 static Window *s_main_window;
-static TextLayer *s_output_layer;
+static TextLayer *s_pq_layer, *s_pq_label_layer;
+static TextLayer *s_average_pq_layer, *s_best_pq_layer, *s_total_punches_layer;
 static double q[10];
-static int qidx = 0;
+static int qidx = 11;
+static int punches = 0;
+static int best_pq = 0;
+static int avg_pq = 0;
+static int total_pq = 0;
 
 static double sqrt( double num )
 {
@@ -31,31 +36,13 @@ static double sqrt( double num )
 }
 
 static void tap_handler(AccelAxisType axis, int32_t direction) {
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Punch detected");
-  switch (axis) {
-  case ACCEL_AXIS_X:
-    if (direction > 0) {
-      text_layer_set_text(s_output_layer, "X axis positive.");
-    } else {
-      text_layer_set_text(s_output_layer, "X axis negative.");
-    }
-    break;
-  case ACCEL_AXIS_Y:
-    if (direction > 0) {
-      text_layer_set_text(s_output_layer, "Y axis positive.");
-    } else {
-      text_layer_set_text(s_output_layer, "Y axis negative.");
-    }
-    break;
-  case ACCEL_AXIS_Z:
-    if (direction > 0) {
-      text_layer_set_text(s_output_layer, "Z axis positive.");
-    } else {
-      text_layer_set_text(s_output_layer, "Z axis negative.");
-    }
-    break;
-  }
+  static char s_buffer[128];
+  text_layer_set_text(s_pq_layer, "Punch Detected!");
+  punches += 1;
   qidx = 0;
+  
+  snprintf(s_buffer, sizeof(s_buffer), "# Punches: %d", punches);
+  text_layer_set_text(s_total_punches_layer, s_buffer);
 }
 
 static int sumq() {
@@ -69,6 +56,8 @@ static int sumq() {
 static void data_handler(AccelData *data, uint32_t num_samples) {
   // Long lived buffer
   static char s_buffer[128];
+  static char s_buffer2[128];
+  static char s_buffer3[128];
   
   double cur_ax = 0;
   double cur_ay = 0;
@@ -84,16 +73,28 @@ static void data_handler(AccelData *data, uint32_t num_samples) {
   cur_ay /= 10.0;
   cur_az /= 10.0;
   
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "LOG");
-  
   if (qidx < 10) {
     q[qidx++] = sqrt(cur_ax * cur_ax + cur_ay * cur_ay + cur_az * cur_az);
   }
   
+  
+  
   if (qidx == 10) {
     
-    snprintf(s_buffer, sizeof(s_buffer), "PQ: %d", sumq());
-    text_layer_set_text(s_output_layer, s_buffer);
+  int pq = sumq();
+  
+  if (pq > best_pq) {
+    best_pq = pq;
+    snprintf(s_buffer2, sizeof(s_buffer2), "Best PQ: %d", best_pq);
+    text_layer_set_text(s_best_pq_layer, s_buffer2);
+  }
+  
+  total_pq += pq;
+  snprintf(s_buffer3, sizeof(s_buffer3), "Avg PQ: %d", total_pq/punches);
+  text_layer_set_text(s_average_pq_layer, s_buffer3);
+    
+    snprintf(s_buffer, sizeof(s_buffer), "%d", pq);
+    text_layer_set_text(s_pq_layer, s_buffer);
     qidx++;
   }
   
@@ -103,17 +104,51 @@ static void main_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect window_bounds = layer_get_bounds(window_layer);
 
-  // Create output TextLayer
-  s_output_layer = text_layer_create(GRect(5, 0, window_bounds.size.w - 10, window_bounds.size.h));
-  text_layer_set_font(s_output_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
-  text_layer_set_text(s_output_layer, "No data yet.");
-  text_layer_set_overflow_mode(s_output_layer, GTextOverflowModeWordWrap);
-  layer_add_child(window_layer, text_layer_get_layer(s_output_layer));
+  // Create PQ Score TextLayer
+  s_pq_layer = text_layer_create(GRect(5, 30, window_bounds.size.w - 10, 30));
+  text_layer_set_font(s_pq_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
+  text_layer_set_text(s_pq_layer, "No data yet.");
+  text_layer_set_overflow_mode(s_pq_layer, GTextOverflowModeWordWrap);
+  text_layer_set_text_alignment(s_pq_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(s_pq_layer));
+  
+  // Create PQ Label
+  s_pq_label_layer = text_layer_create(GRect(5, 5, window_bounds.size.w - 10, 30));
+  text_layer_set_font(s_pq_label_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+  text_layer_set_text(s_pq_label_layer, "Last PQ:");
+  text_layer_set_overflow_mode(s_pq_label_layer, GTextOverflowModeWordWrap);
+  text_layer_set_text_alignment(s_pq_label_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(s_pq_label_layer));
+  
+  s_best_pq_layer = text_layer_create(GRect(5, 65, window_bounds.size.w - 10, 30));
+  text_layer_set_font(s_best_pq_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+  text_layer_set_text(s_best_pq_layer, "Best PQ: 0");
+  text_layer_set_overflow_mode(s_best_pq_layer, GTextOverflowModeWordWrap);
+    text_layer_set_text_alignment(s_best_pq_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(s_best_pq_layer));
+  
+  s_average_pq_layer = text_layer_create(GRect(5, 90, window_bounds.size.w - 10, 30));
+  text_layer_set_font(s_average_pq_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
+  text_layer_set_text(s_average_pq_layer, "Avg PQ: 0");
+  text_layer_set_overflow_mode(s_average_pq_layer, GTextOverflowModeWordWrap);
+    text_layer_set_text_alignment(s_average_pq_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(s_average_pq_layer));
+  
+  s_total_punches_layer = text_layer_create(GRect(5, 120, window_bounds.size.w - 10, 30));
+  text_layer_set_font(s_total_punches_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
+  text_layer_set_text(s_total_punches_layer, "# Punches: 0");
+  text_layer_set_overflow_mode(s_total_punches_layer, GTextOverflowModeWordWrap);
+  text_layer_set_text_alignment(s_total_punches_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(s_total_punches_layer));
+  
+ 
+  
+ 
 }
 
 static void main_window_unload(Window *window) {
   // Destroy output TextLayer
-  text_layer_destroy(s_output_layer);
+  text_layer_destroy(s_pq_layer);
 }
 
 static void init() {
